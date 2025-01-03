8ea83bdcc03b12ff6f321a03a050475a3395750b
teikv <levankiet1212.2004@gmail.com>
2025-01-03 15:12:01 +0700

    Part 2 Section 4

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,7 +1,17 @@
 <x-user-layout>
     @section('title', 'Homepage')
 
+    <x-slot name="navigation">
+        <x-navigation :categories="$categories" />
+    </x-slot>
+    
     @foreach ($categories as $category)
-        <x-product-grids :category="$category" :products="$category['products']" />
+        <x-product-grids :category="$category" :products="$category['products']"/>
     @endforeach
+
+    <x-slot name="components">
+        <livewire:checkout.cart-bar />
+        
+        <livewire:checkout.add-cart-item-popup />
+    </x-slot>
 </x-user-layout>
